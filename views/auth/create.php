2137a3a8083ab37480429716fc814dbf35ac61cf
Diego Suárez <diego.suarezg96@gmail.com>
2022-10-18 23:58:50 -0500

    fix action page and input value

--- views/auth/create.php
+++ views/auth/create.php
@@ -4,7 +4,7 @@
     <div class="contenedor-sm">
         <p class="descripcion-pagina">Crea tu cuenta en UpTask</p>
 
-        <form class="formulario" action="/" method="POST">
+        <form class="formulario" action="/create" method="POST">
             <div class="campo">
                 <label for="nombre">Nombre:</label>
                 <input type="text" name="nombre" id="nombre" placeholder="tu nombre">
@@ -25,7 +25,7 @@
                 <input type="password" name="password2" id="password2" placeholder="repite tu contraseña">
             </div>
 
-            <input type="submit" value="Iniciar sesión" class="boton">
+            <input type="submit" value="Crear cuenta" class="boton">
         </form>
 
         <div class="acciones">
