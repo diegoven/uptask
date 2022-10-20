a2da5d6c4221f66523949094eb89b0f9f2ed9086
Diego Suárez <diego.suarezg96@gmail.com>
2022-10-19 23:46:37 -0500

    displays the list of projects on the dashboard

--- controllers/DashboardController.php
+++ controllers/DashboardController.php
@@ -12,8 +12,11 @@ class DashboardController
         session_start();
         isAuth();
 
+        $proyectos = Proyecto::belongsTo('propietarioId', $_SESSION['id']);
+
         $router->render('dashboard/index', [
-            'titulo' => 'Proyectos'
+            'titulo' => 'Proyectos',
+            'proyectos' => $proyectos
         ]);
     }
 
--- views/dashboard/index.php
+++ views/dashboard/index.php
@@ -1,5 +1,17 @@
 <?php include_once __DIR__ . "/header-dashboard.php"; ?>
 
-
+<?php if (count($proyectos) === 0) { ?>
+    <p class="no-proyectos">Aún no has creado proyectos... <a href="/create-project">¡Comienza creando uno!</a></p>
+<?php } else { ?>
+    <ul class="listado-proyectos">
+        <?php foreach ($proyectos as $proyecto) : ?>
+            <li>
+                <a class="proyecto" href="/project?id=<?php echo $proyecto->url; ?>">
+                    <?php echo $proyecto->proyecto; ?>
+                </a>
+            </li>
+        <?php endforeach; ?>
+    </ul>
+<?php } ?>
 
 <?php include_once __DIR__ . "/footer-dashboard.php"; ?>
